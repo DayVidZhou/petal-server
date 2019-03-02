9516968fda8e63466b2bdff93b16d9359c465087
Nathaniel Ayalew <n2ayalew@edu.uwaterloo.ca>
2019-03-02 13:00:58 -0500

    print post data to confirm curl posts are correct

--- app/routes.py
+++ app/routes.py
@@ -4,43 +4,56 @@ from flask import request, jsonify, redirect, url_for
 @app.route('/', methods = ["GET","POST"])
 @app.route('/index', methods = ["GET","POST"])
 def measurement():
-	try:
-		if request.method == "POST":
+    if request.method == 'POST':
+        print(request.method)
+        print(request.form.get('time'))
+        print(request.form.get('current1'))
+        print(request.form.get('realP2'))
+        return 'thanks for measurments'
 
-			print(request.data)
-			return request.data
-			if request.is_json:
-				json = request.get_json()
-				formattedTime = dt.datetime.utcfromtimestamp(json['time']).strftime("%Y-%m-%d %H:%M:%S.%f")
-				measurement = Measurement(time=formattedTime, current=json['current']\
-					, voltage=json['voltage'], realp=json['realp'])
-				db.session.add(measurement)
-				db.session.commit()
-				return jsonify(json)
-		if request.method == "GET":
-			return "OH YOU JUST TRYNA READDDD!!!!"
-	except:
-		return "SOMETHING WENT WRONG"
+"""
+def measurement():
+    print(request.form.keys()[0])
+    print(request.form.get('time'))
+        try:
+            if request.method == "POST":
+                print(request.form.keys()[0])
+                        print(request.form.get('time'))
+                        print(request.data)
+                        return request.data
+                    if request.is_json:
+                        json = request.get_json()
+                                formattedTime = dt.datetime.utcfromtimestamp(json['time']).strftime("%Y-%m-%d %H:%M:%S.%f")
+                                measurement = Measurement(time=formattedTime, current=json['current']\
+                                        , voltage=json['voltage'], realp=json['realp'])
+                                db.session.add(measurement)
+                                db.session.commit()
+                                return jsonify(json)
+                if request.method == "GET":
+                    return "OH YOU JUST TRYNA READDDD!!!!"
+        except:
+            return "SOMETHING WENT WRONG"
 
 
 @app.route('/testing', methods = ["GET","POST"])
 def test():
-	if request.get_json():
-		json = request.get_json()
-		test = Test(tests=json['tests'], testf=json['testf'])
-		db.session.add(test)
-		db.session.commit()
-		return jsonify(json)
-	else:
-		return "OH YOU wanna read TESTSTSS!!!!"
+    if request.get_json():
+        json = request.get_json()
+                test = Test(tests=json['tests'], testf=json['testf'])
+                db.session.add(test)
+                db.session.commit()
+                return jsonify(json)
+        else:
+            return "OH YOU wanna read TESTSTSS!!!!"
 
 @app.route('/measurements', methods = ["GET"])
 def getMeasurements():
-	measurements = Measurements.query.all()
-	measure_list = {}
-	i = 1
-	for x in measurements:
-		measure_list[i] = x.realp
-		i += 1
-		print(measure_list)
-	return jsonify(measure_list)
+    measurements = Measurements.query.all()
+        measure_list = {}
+        i = 1
+        for x in measurements:
+            measure_list[i] = x.realp
+                i += 1
+                print(measure_list)
+        return jsonify(measure_list)
+"""
