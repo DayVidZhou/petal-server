ca893e99cebccad3dfffa7e1100919721037d4a3
David Zhou <dszhou@Davids-MacBook-Pro.local>
2019-03-02 19:30:04 -0500

    can take measurement now

--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,18 @@ from flask import request, jsonify, redirect, url_for
 def measurement():
 	try:
 		if request.method == "POST":
-
-			print(request.data)
-			return request.data
-			if request.is_json:
-				json = request.get_json()
-				formattedTime = dt.datetime.utcfromtimestamp(json['time']).strftime("%Y-%m-%d %H:%M:%S.%f")
-				measurement = Measurement(time=formattedTime, current=json['current']\
-					, voltage=json['voltage'], realp=json['realp'])
-				db.session.add(measurement)
-				db.session.commit()
-				return jsonify(json)
+			print(request.form)
+			measurement = Measurement(time = request.form.get('time'), current1 = request.form.get('current1'), \
+				voltage = request.form.get('voltage'), current2 = request.form.get('current2'), \
+				realP1 = request.form.get('realP1'), realP2 = request.form.get('realP2'))
+			# if request.is_json:
+			# 	json = request.get_json()
+			# 	formattedTime = dt.datetime.utcfromtimestamp(json['time']).strftime("%Y-%m-%d %H:%M:%S.%f")
+			# 	measurement = Measurement(time=formattedTime, current=json['current']\
+			# 		, voltage=json['voltage'], realp=json['realp'])
+			db.session.add(measurement)
+			db.session.commit()
+        	return 'thanks for measurments'
 		if request.method == "GET":
 			return "OH YOU JUST TRYNA READDDD!!!!"
 	except:
