4836331b416a2813a3c30eaa4322295d12947e98
David Zhou <dszhou@Davids-MacBook-Pro.local>
2019-03-03 02:46:37 -0500

    fixed db maybe

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
 app.config.from_object(Config)
-heroku = Heroku(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
